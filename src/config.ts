c09df4f75fefc5fd987882f21f1bf407261bb81b
lingmeng <wxzjt0318@gmail.com>
2026-01-19 11:53:42 +0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -123,8 +123,8 @@ export const siteConfig: SiteConfig = {
 
         // PicFlow API支持(智能图片API)
         imageApi: {
-            enable: false, // 启用图片API
-            url: "http://domain.com/api_v2.php?format=text&count=4", // API地址，返回每行一个图片链接的文本
+            enable: true, // 启用图片API
+            url: "https://lm520.cc/api?type=scenery&format=text&num=6", // API地址，返回每行一个图片链接的文本
         },
         // 这里需要使用PicFlow API的Text返回类型,所以我们需要format=text参数
         // 项目地址:https://github.com/matsuzaka-yuki/PicFlow-API
@@ -602,6 +602,7 @@ export const umamiConfig = {
 <script defer src="XXXX.XXX" data-website-id="ABCD1234"></script>
   `.trim(), // 上面填你要插入的Script,不用再去Layout中插入
 } as const;
+
 
 
 
